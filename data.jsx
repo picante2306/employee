eb7946ac03ded574165f226f1fa639888a37c039
picante2306 <51654819+picante2306@users.noreply.github.com>
2024-12-27 15:47:38 +0530

    data.jsx

---
+++ data.jsx
@@ -0,0 +1,17 @@
+export const userlist = [
+    {
+        name:"John",
+        email:"john32@gmail.com",
+        id:1
+    },
+    {
+        name:"Doe",
+        email:"doee43@gmail.com",
+        id:2
+    },
+    {
+        name:"Jane",
+        email:"jane78@hotmail.com",
+        id:3
+    }
+]
